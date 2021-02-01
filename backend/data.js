685929f3a4cb78f3be258d42439c3142bd11e95a
Tarique <tariqueansari455@gmail.com>
2021-02-01 20:54:08 +0530

    Backend starts

---
+++ backend/data.js
@@ -0,0 +1,30 @@
+const data ={
+Products:[
+    {
+        _id:'1',
+        name:"Over Brand",
+        price:11,
+        image:"./images/OverBrand.jpg"
+    },
+    {
+        _id:'2',
+        name:"Over Brand",
+        price:11,
+        image:"./images/OverBrand.jpg"
+    },
+    {
+        _id:'3',
+        name:"Over Brand",
+        price:11,
+        image:"./images/OverBrand.jpg"
+    },
+    {
+        _id:'4',
+        name:"Over Brand",
+        price:11,
+        image:"./images/OverBrand.jpg"
+    },
+]
+};
+
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import data from './data.js';
+// const app = express();
+
+const app=express();
+
+app.get('/api/products',(req,res)=>{
+    res.send(data.Products);
+});
+
+app.get('/',(req,res)=>{
+    res.send("server is ready");
+});
+app.listen(5000,(req,res)=>{
+    console.log("Server running at localhost:5000");
+});
