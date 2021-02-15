867fa79da597e64427ff4719e3c78592cf485631
Tarique <tariqueansari455@gmail.com>
2021-02-15 11:16:36 +0530

    First Realse V.0.0.0.1

--- clientside/src/pages/LoadingBox.js
+++ clientside/src/pages/LoadingBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LoadingBox() {
     return (
         <div>
-            loading
+            <i className="fas fa-spinner"></i>
         </div>
     )
 }
--- clientside/src/template/header.js
+++ clientside/src/template/header.js
@@ -2,15 +2,14 @@ import React from "react";
 // import SideBar from "./sideBar";
 
 class Header extends React.Component {
-        
-        render(){
+  render(){
         return (
             <div className="container-fluid main_menu">
   <div className="row">
     <div className="col-md-10 col-12 mx-auto">
       <nav className="navbar navbar-expand-lg"> 
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">MeraGhar<span className="dom">.online</span></a>
+    <a className="navbar-brand" href="/">SubKaGhar<span className="dom">.online</span></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
