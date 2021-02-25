af679d518597baa23c5223182179383d3cdf36c6
Tarique <tariqueansari455@gmail.com>
2021-02-25 12:44:20 +0530

    Deployment issue 0.2

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,18 @@
 import  express from 'express';
 import data from "./data.js";
 // require('../router/signUproute.js');
-
+// import path from 'path';
 const PORT=process.env.PORT || 5000;
 const app= express();
 
+// app.use('/', express.static(path.join(__dirname,'dist')));
 
 app.get('/api/products',(req,res)=>{
     res.send(data.Products);
 })
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 // app.use("/auth",router);
 app.listen(PORT,()=>{
     console.log(`Server running on Port${PORT}`);
